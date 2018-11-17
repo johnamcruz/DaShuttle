a1f27046bdcbcd1f5e3eb6fd119777a19ce4c34e
John Cruz <johnamcruz@gmail.com>
2018-11-17 12:29:40 -1000

    intial commit

---
+++ DaShuttle/DaShuttle/Controllers/MapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MapViewController.swift
+//  DaShuttle
+//
+//  Created by John M Cruz on 11/17/18.
+//  Copyright © 2018 John M Cruz. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
